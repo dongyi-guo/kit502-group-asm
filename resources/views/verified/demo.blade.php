b53c3fd7164a4286e2e96c6e9441b550668096e6
razi061 <129680513+razi061@users.noreply.github.com>
2023-05-22 03:33:21 +1000

    Dashboard

--- resources/views/verified/demo.blade.php
+++ resources/views/verified/demo.blade.php
@@ -3,21 +3,109 @@
 @section('title', 'Demo Page')
  
 @section('content')
-    <h4>Terms and Conditions</h4>
-    <p class="justify-text">These terms and conditions explain in detail your rights and obligations when using TaGET and its features, including accessing the digital web (browser URL) and mobile interfaces.</p>
-    <p class="justify-text">TaGET is available to all eligible residential and business customers on a Standard Retail Contract.</p>
-    <p class="justify-text">These terms and conditions do not cover the sale or supply of energy to you at your premises or related matters which are covered in your Standard Retail Contract.</p>
-    <p class="justify-text">More information about TaGET, your Standard Retail Contract and other matters can be found on our website taget.com.au.</p>
-    <br />
-    <h4>Use of TaGET</h4>
-    <div>
-        <ul>
-            <li class="justify-text">We, TaGET Energy, are the owner and operate its key features, including the web and mobile interfaces. We may, at our sole discretion, refuse to provide you with access to aurora+, including where you fail to satisfy the pre-conditions.</li>
-            <li class="justify-text">You agree to be bound by these terms and conditions. Your use of TaGET and access to its features indicates your acceptance of and agreement with these terms and conditions, as they exist at the time of use.</li>
-            <li class="justify-text">We may amend these terms and conditions at any time. If we do so, we will endeavour to provide notice as soon as practicable that changes have been made (e.g. on our website). If you use aurora+ or access its features after a notice of change has been provided, you will be deemed to have accepted the new terms and conditions.</li>
-            <li class="justify-text">You agree that use of TaGET requires compatible devices and internet access, may require certain software or operating systems, may require periodic updates, and may be affected by the performance of these factors. You agree that meeting these requirements, which may change from time to time, is your responsibility.</li>
-            <li class="justify-text">These terms and conditions do not affect your rights and obligations under your Standard Retail Contract.</li>
-            <li class="justify-text">You are not required to pay TaGET Energy any separate fees or charges for the use of TaGET, including the web and mobile interfaces.  However, you may incur charges from your mobile service provider for downloading and using the application. More information on Aurora Energy’s prices can be found on our website taget.com.au.</li>
-        </ul>
-    </div>  
+    <h1 class="text-center my-2">Trading Market Summary</h1>
+    <div class='row'>
+        <div class="col-sm-12 col-md-6 my-4" id="curve_chart"></div>
+        <div class="col-sm-12 col-md-6 my-4" id="piechart"></div>
+    </div>
+    <h1 class="text-center my-2">System Summary</h1>
+    <div class='row'>
+        <div class="col-sm-12 col-md-6 my-4" id="curve_chart2"></div>
+        <div class="col-sm-12 col-md-6 my-4" id="columnchart_values"></div>
+    </div>
 @stop
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawLineChart);
+      google.charts.setOnLoadCallback(drawPieChart);
+      google.charts.setOnLoadCallback(drawLineChart2);
+      google.charts.setOnLoadCallback(drawColumnChart);
+
+      function drawLineChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Day', 'Solar', 'Wind'],
+          ['2023-02-08', 1.8, 1.6],
+          ['2023-02-09', 2.0, 1.4],
+          ['2023-02-10', 1.4, 1.5],
+          ['2023-02-11', 2.0, 1.9],
+          ['2023-02-14', 1.5, 2.0]
+        ]);
+
+        var options = {
+          title: 'Market Price',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+        chart.draw(data, options);
+      }
+
+      function drawPieChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Product', 'Market Share'],
+          ['Solar Sell', 28],
+          ['Solar Buy', 24],
+          ['Wind Sell', 15],
+          ['Wind Buy', 8]
+        ]);
+
+        var options = {
+          title: 'Trading Amount'
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+        chart.draw(data, options);
+      }
+
+      function drawLineChart2() {
+        var data = google.visualization.arrayToDataTable([
+          ['Day', 'Tax', 'Administration Fee'],
+          ['2023-02-09', 10, 20],
+          ['2023-02-10', 10, 10],
+          ['2023-02-11', 5.2, 10],
+          ['2023-02-12', 5, 10],
+          ['2023-02-13', 5, 15],
+          ['2023-02-14', 5, 18]
+        ]);
+
+        var options = {
+          title: 'Service and Tax',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart2'));
+
+        chart.draw(data, options);
+      }
+
+      function drawColumnChart() {
+        var data = google.visualization.arrayToDataTable([
+            ["User Type", "User Count", { role: "style" } ],
+            ["Both", 5, "silver"],
+            ["Buyer", 5, "silver"],
+            ["Seller", 10, "silver"],
+            ["SM", 2, "silver"]
+        ]);
+
+        var view = new google.visualization.DataView(data);
+        view.setColumns([0, 1,
+                        { calc: "stringify",
+                            sourceColumn: 1,
+                            type: "string",
+                            role: "annotation" },
+                        2]);
+
+        var options = {
+            title: "User",
+            bar: {groupWidth: "95%"},
+            legend: { position: "none" },
+        };
+        var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
+        chart.draw(view, options);
+    }      
+</script>
+
--- resources/views/verified/layout.blade.php
+++ resources/views/verified/layout.blade.php
@@ -6,6 +6,8 @@
         <link rel="stylesheet" href="{{ asset('css/style.css') }}">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.2.3/animate.min.css" />
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     </head>
     <body>
         <div class="container">
@@ -20,8 +22,7 @@
         </div>
     </body>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/js/all.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script sr="https://cdnjs.cloudflare.com/ajax/libs/wow/1.1.2/wow.min.js" integrity="sha512-Eak/29OTpb36LLo2r47IpVzPBLXnAMPAVypbSZiZ4Qkf8p/7S/XRG5xp7OKWPPYfJT6metI+IORkR5G8F900+g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </html>
